57bb02e755105a566754be2f7d3caae9e5170a06
arielbakal <arielbakal.dev@gmail.com>
2023-09-07 17:01:18 -0300

    G2 : init resolucion

---
+++ guia2.R
@@ -0,0 +1,4 @@
+# 1.1: Categoricas = Color, Nombre, Pais | Numericas = Edad, Peso, Altura
+# 1.2: gdpPercap es una variable numerica continua porque toma valores reales
+# 1.3: I.gdp es numerica discreta porque toma valores enteros. Si, pasaria a ser categorica ordinal, ya que establecemos cierto orden jerarquico.
+
