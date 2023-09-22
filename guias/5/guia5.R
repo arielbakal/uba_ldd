0955acf8a7c8e58cc6bf34b249266ab226dc273c
arielbakal <arielbakal.dev@gmail.com>
2023-09-22 12:32:23 -0300

    G5 : Completo

--- guias/5/guia5.R
+++ guias/5/guia5.R
@@ -259,6 +259,7 @@ for(i in 1:Nrep){
 
 # calcula la probabilidad estimada de coincidencias y la imprime en la consola
 p_coincidencias <- n_coincidencias / Nrep
+
 print(p_coincidencias)
 
 pcumples <- function(N=50, Nrep=10000){
@@ -293,20 +294,182 @@ ggplot(p_c) +
   geom_line(mapping = aes(y = Prob, x = Grupo))
 
 # 4.3
+n <- 500 # numero de votos para A
+m <- 400 # numero de votos para B
+votos <- c()
+for (i in 1:n) {votos <- append(votos, 1)}
+for (i in 1:m) {votos <- append(votos, -1)}
+votos <- sample(votos)
 
+resultado <- cumsum(votos)
+for (i in 1:length(resultado)) {
+  if (resultado[i] == -1) {
+    A_gana_siempre <- FALSE
+  }
+}
+A_gana_siempre
 
+cuenta <- 0
+Nrep   <- 1000
+for (i in 1:Nrep){
+  A_gana_siempre <- TRUE
+  escrutinio <- sample(votos)
+  resultado  <- cumsum(escrutinio)
+  for (i in 1:length(escrutinio)) {
+    if (resultado[i] == -1) {
+      A_gana_siempre <- FALSE
+      break
+    }
+  }
+  if (A_gana_siempre == TRUE){
+    cuenta <- cuenta + 1
+  }
+}
+p <- cuenta / Nrep
+print(p)
+print((n-m)/(n+m))
 
+n <- 505 # numero de votos para A
+m <- 495 # numero de votos para B
+votos <- c( rep(1,n), rep(-1,m) )
 
+# cuentaA va a contar cuántas veces ocurre que A va ganando 
+# hasta contar 800 votos
+cuentaA <- 0  
+cuentaB <- 0  # esta hace lo mismo para B
+Nrep   <- 10000
+for (i in 1:Nrep){
+  A_gana_siempre <- TRUE
+  B_gana_siempre <- TRUE
+  escrutinio <- sample(votos, n+m, replace = FALSE)
+  resultado  <- cumsum(escrutinio)
+  for (i in 1:800){
+    if (resultado[i] == -1){
+      A_gana_siempre <- FALSE
+    }
+    if (resultado[i] > 0 ){  
+      B_gana_siempre <- FALSE
+    }
+  }
+  if (A_gana_siempre == TRUE){
+    cuentaA <- cuentaA + 1
+  } else {
+    cuentaB <- cuentaB + 1
+  }
+}
 
+pA <- cuentaA / Nrep
+pB <- cuentaB / Nrep
+print(pA)
+print(pB)
 
+"que no habria votos en blancos?"
 
+# 4.4
+N  <- 10   # numero total de personas
+n1 <- 4    # numero de personas que inicialmente opinan +1
+Op <- c( rep(1, n1), sample(c(1,-1), N - n1, replace = TRUE))
 
+n_int     <- sample(1:N, 3, replace = FALSE)
+Op[n_int] <- ifelse(sum(Op[n_int]) == 1, 1, -1 )
 
+Op <- c( rep(1, n1), sample(c(1,-1), N - n1, replace = TRUE))
+consenso <- 0
+while( consenso != 1 ){
+  
+  n_int     <- sample(1:N, 3, replace = FALSE)
+  Op[n_int] <- ifelse(sum(Op[n_int]) > 0, 1, -1 )
+  
+  if (sum(Op) == length(Op)){
+    consenso <- 1
+    Oconsenso <- 1
+  }
+  if (sum(Op) == -length(Op)){
+    consenso <- 1
+    Oconsenso <- -1
+  }
+}
+Oconsenso
 
+p_consenso <- function(N, n1){
+  
+  output   <- 0
+  
+  for (i in 1:100){
+    
+    Op <- c( rep(1, n1), sample(c(1,-1), N - n1, replace = TRUE))
+    consenso <- 0
+    while( consenso != 1 ){
+      
+      n_int     <- sample(1:N, 3, replace = FALSE)
+      Op[n_int] <- ifelse(sum(Op[n_int]) > 0, 1, -1 )
+      
+      if (sum(Op) == length(Op)){
+        consenso <- 1
+        Oconsenso <- 1
+      }
+      if (sum(Op) == -length(Op)){
+        consenso <- 1
+        Oconsenso <- -1
+      }
+    }
+    if (Oconsenso == 1){
+      output <- output + 1
+    }
+  }
+  return(output/100)
+}
+p_consenso(100, 10)
 
+p_consensos <- c()
+for (i in 1:10) {p_consensos <- append(p_consensos, p_consenso(100, i))}
+p_consensos
 
+p_consensos <- data.frame(porcentajes = p_consensos)
 
+p_consensos$iniciantes <- seq_along(p_consensos$porcentajes)
 
+require(ggplot2)
+ggplot(p_consensos) +
+  geom_line(mapping = aes(x = iniciantes, y = porcentajes))
 
+# 4.5
+monty_hall <- function(n_puerta, mantiene_puerta){
+  puertas <- sample(c(TRUE, FALSE, FALSE))
+  
+  eleccion <- puertas[n_puerta]
+  
+  indices_restantes <- c(1,2,3)[-eleccion_indice]
+  
+  if (puertas[indices_restantes[1]] == FALSE) {
+    indices_restantes <- indices_restantes[-indices_restantes[1]]
+  } else {
+    indices_restantes <- indices_restantes[-indices_restantes[2]]
+  }
+  
+  mantiene <- mantiene_puerta
+  
+  if (mantiene == TRUE){
+    eleccion <- eleccion
+  } else {
+    eleccion <- puertas[indices_restantes]
+  }
+  return(eleccion)
+}
+
+monty_hall(1,FALSE)
+
+prob_monty_hall <- function(iteraciones, mantiene_puerta){
+  cuenta <- 0
+  for (i in 1:iteraciones){
+    puerta <- sample(c(1,2,3), size = 1)
+    if (monty_hall(puerta, mantiene_puerta) == TRUE){
+      cuenta <- cuenta + 1
+    }
+  }
+  return(cuenta/iteraciones)
+}
+prob_monty_hall(100000, TRUE)
+prob_monty_hall(100000, FALSE)
 
 
